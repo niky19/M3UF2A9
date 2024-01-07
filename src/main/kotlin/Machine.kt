b26dd31e4e476ff1f88ebc414b1ea17a5cb1eb1c
niky <nikita.barbosa.7e5@itb.cat>
2024-01-07 20:09:34 +0100

    update

--- src/main/kotlin/Machine.kt
+++ src/main/kotlin/Machine.kt
@@ -1,9 +1,18 @@
 import java.util.*
+import kotlin.system.exitProcess
 
+/**
+ * Valors multiplicadors per calcular el preu del bitllet segons la zona i llista de monedes i bitllets acceptats.
+ */
 const val zona2 = 1.3125
 const val zona3 = 1.8443
 val allowedMoney = listOf(0.05, 0.10, 0.20, 0.50, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0)
 
+/**
+ * Crea una lista de tiquets disponibles mitjançant la classe Ticket.
+ * @return Llista de tiquets disponibles.
+ * @author Daniel Reinosa, Joel Montalvan, Nikita Barbosa
+ */
 fun createTicketList(): List<Ticket> {
     return listOf(
         Ticket("Bitllet Senzill", 2.40),
@@ -14,10 +23,24 @@ fun createTicketList(): List<Ticket> {
     )
 }
 
+/**
+ * Comprova si la moneda o bitllet introduït per l'usuari és vàlid (està a la llista de monedes i bitllets acceptats).
+ * @param userMoney: Moneda o bitllet introduït per l'usuari.
+ * @return boolean indicant si la moneda o bitllet és vàlid o no.
+ * @author Daniel Reinosa, Joel Montalvan, Nikita Barbosa
+ */
 fun isMoneyAllowed(userMoney: Double): Boolean {
     return allowedMoney.contains(userMoney)
 }
 
+
+/**
+ * Efectua les operacions necessàries per processar la compra del bitllet amb un while que s'executa mentre l'usuari no hagi introduït l'import total. Si l'usuari introdueix un import superior al preu del bitllet, es calcula el canvi.
+ * @param scanner: per a l'input de l'usuari.
+ * @param price: el preu del bitllet.
+ * @return Double amb els diners introduïts per l'usuari.
+ * @author Daniel Reinosa, Joel Montalvan, Nikita Barbosa
+ */
 fun payment(scanner: Scanner, price: Double): Double {
     var money = 0.0
     while (money < price) {
@@ -36,12 +59,26 @@ fun payment(scanner: Scanner, price: Double): Double {
     return money
 }
 
+/**
+ * Calcula el canvi a retornar a l'usuari.
+ * @param money: diners introduïts per l'usuari.
+ * @param price: preu del bitllet.
+ * @return Double amb el canvi a retornar a l'usuari.
+ * @author Daniel Reinosa, Joel Montalvan, Nikita Barbosa
+ */
 fun calculateChange(money: Double, price: Double): Double {
     val canvi = money - price
     println("Recull el teu canvi: $canvi€.")
     return canvi
 }
 
+
+/**
+ * Printa el tiquet de compra si ho desitja l'usuari.
+ * @param completedTickets: llista de tiquets comprats per l'usuari.
+ * @param scanner: per a la resposta de l'usuari.
+ * @author Daniel Reinosa, Joel Montalvan, Nikita Barbosa
+ */
 //todo hacer que se vea bonito y poner fecha
 fun printTickets(completedTickets: MutableList<CompletedPurchase>, scanner: Scanner){
     println("Desitja tiquet en paper? (S/N): ")
@@ -56,6 +93,11 @@ fun printTickets(completedTickets: MutableList<CompletedPurchase>, scanner: Scan
     }
 }
 
+/**
+ * Calcula el preu total de la transacció. Recorre la llista de tiquets comprats i suma el preu de cada tiquet amb la funció calculatePrice, que pertany a la classe Ticket.
+ * @param completedTickets: llista de tiquets comprats per l'usuari.
+ * @return Double amb el preu total de la compra.
+ */
 fun calculateTotalPrice(completedTickets: MutableList<CompletedPurchase>): Double {
     var totalPrice = 0.0
    for(ticket in completedTickets){
@@ -63,6 +105,13 @@ fun calculateTotalPrice(completedTickets: MutableList<CompletedPurchase>): Doubl
    }
     return totalPrice
 }
+
+/**
+ * Funció per a la selecció del tiquet a comprar fent servir una funció que limita els inputs al rang d'opcions disponibles.
+ * @param scanner: per a l'input de l'usuari.
+ * @param ticketList: llista de tiquets disponibles.
+ * @return Tiquet seleccionat per l'usuari.
+ */
 fun selectTicket(scanner: Scanner, ticketList: List<Ticket>): Ticket {
     println(
         """
@@ -79,6 +128,11 @@ fun selectTicket(scanner: Scanner, ticketList: List<Ticket>): Ticket {
     return selectedTicket
 }
 
+/**
+ * Selecció de la zona fent servir una funció que limita els inputs al rang d'opcions disponibles.
+ * @param scanner: per a l'input de l'usuari.
+ * @return Int amb la zona seleccionada per l'usuari.
+ */
 fun selectZone(scanner: Scanner): Int {
     println(
         """
@@ -92,6 +146,13 @@ fun selectZone(scanner: Scanner): Int {
     return userZone
 }
 
+
+/**
+ * Compra del bitllet. Crida a les funcions selectTicket, selectZone, payment i calculateChange.
+ * @param scanner: per a l'input de l'usuari.
+ * @param ticketList: llista de tiquets disponibles.
+ * @return CompletedPurchase amb el tiquet comprat i la zona seleccionada per l'usuari.
+ */
 fun purchaseTicket (scanner: Scanner, ticketList: List<Ticket>): CompletedPurchase {
     val userTicket = selectTicket(scanner, ticketList)
     val userZone = selectZone(scanner)
@@ -102,6 +163,11 @@ fun purchaseTicket (scanner: Scanner, ticketList: List<Ticket>): CompletedPurcha
     return CompletedPurchase(userTicket, userZone)
 }
 
+/**
+ * Funció principal que inicia la màquina. Crida a les funcions createTicketList, initMachine i printTickets.
+ * @param scanner: per a l'input de l'usuari
+ * @param ticketList: llista de tiquets disponibles.
+ */
 fun initMachine(scanner: Scanner, ticketList: List<Ticket>) {
     val userCart = createCart()
     var continueShopping = true
@@ -117,6 +183,7 @@ fun initMachine(scanner: Scanner, ticketList: List<Ticket>) {
     endPurchase(userCart, scanner)
 }
 
+/
 fun endPurchase(userCart: MutableList<CompletedPurchase> ,scanner: Scanner){
     printTickets(userCart, scanner)
     println("Gràcies per utilitzar la màquina de venda. Adéu!")
